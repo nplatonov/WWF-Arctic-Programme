acc356bbcf7bf3828aacc2b7d00b062bd121b952
WWF-Arctic-Programme <msommerkorn@wwf.no>
2022-12-15 13:55:21 +0300

    ongoing

--- _config.bat
+++
@@ -1,4 +0,0 @@
-::git config --global --list
-::git config --local user.name "WWF-Arctic-Programme"
-::git config --local user.email "msommerkorn@wwf.no"
-::git config --local --list
--- _upload.R
+++
@@ -1,8 +0,0 @@
-if (T) {
-   Sys.setenv(GITHUB_PAT="")
-   ret <- c(add=999,commit=999,push=999)
-   ret[1] <- system("git add -A")
-   if (!(ret[2] <- system("git commit -m\"ongoing\"")))
-      ret[3] <- system("git push")
-   print(ret)
-}
