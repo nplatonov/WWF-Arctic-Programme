30bdb9919516d0184e1df41f57a2d0b0f0b0a02e
WWF-Arctic-Programme <msommerkorn@wwf.no>
2022-12-15 13:13:57 +0300

    ongoing

---
+++ _config.bat
@@ -0,0 +1,4 @@
+::git config --global --list
+::git config --local user.name "WWF-Arctic-Programme"
+::git config --local user.email "msommerkorn@wwf.no"
+::git config --local --list
---
+++ _upload.R
@@ -0,0 +1,7 @@
+if (T) {
+   ret <- c(add=999,commit=999,push=999)
+   ret[1] <- system("git add -A")
+   if (!(ret[2] <- system("git commit -m\"ongoing\"")))
+      ret[3] <- system("git push")
+   print(ret)
+}
